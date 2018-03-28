71ca865fb7b7f2908d0d6d7c0173dd4fa85bc1c5
Graham Watson <graham@phantomwatson.com>
2018-03-28 18:51:58 -0400

    Start writing run() and add year and fileKey selection

--- src/Shell/ImportShell.php
+++ src/Shell/ImportShell.php
@@ -4,8 +4,25 @@ namespace App\Shell;
 use App\Import\Import;
 use Cake\Console\Shell;
 
+/**
+ * Class ImportShell
+ * @package App\Shell
+ * @property Import $import
+ */
 class ImportShell extends Shell
 {
+    private $import;
+
+    /**
+     * Initializes the Shell
+     *
+     * @return void
+     */
+    public function initialize()
+    {
+        parent::initialize();
+        $this->import = new Import();
+    }
 
     /**
      * Display help for this console.
@@ -15,11 +32,13 @@ class ImportShell extends Shell
     public function getOptionParser()
     {
         $parser = parent::getOptionParser();
-        $parser->addSubcommand('status', [
+        $parser->addSubcommand('run', [
+            'help' => 'Process file(s) and import data',
+        ])->addSubcommand('status', [
             'help' => 'Show what files are available and which have been imported',
-        ])->addOption('year', [
-            'help' => 'The specific year to look up',
-            'default' => null
+        ])->addArguments([
+            'year' => ['help' => 'The specific year to look up'],
+            'fileKey' => ['help' => 'The numeric key for the specific file to process']
         ]);
 
         return $parser;
@@ -33,8 +52,7 @@ class ImportShell extends Shell
      */
     public function status($year = null)
     {
-        $import = new Import();
-        $files = $import->getFiles();
+        $files = $this->import->getFiles();
 
         if ($year) {
             if (isset($files[$year])) {
@@ -51,5 +69,68 @@ class ImportShell extends Shell
             $this->helper('Table')->output($yearFiles);
             $this->out('');
         }
+    }
+
+    /**
+     * Collects a year/subdirectory
+     *
+     * @return int
+     */
+    private function selectYear()
+    {
+        $availableYears = $this->import->getYears();
+        $year = null;
+        while (!in_array($year, $availableYears)) {
+            $year = $this->in('Select a year (' . min($availableYears) . '-' . max($availableYears) . '):');
+        }
+
+        return (int)$year;
+    }
+
+    /**
+     * Collects a file key for the specified key from the user
+     *
+     * @param int $year The year/subdirectory to select a file from
+     * @return int
+     */
+    private function selectFileKey($year)
+    {
+        $files = $this->import->getFiles();
+        if (!isset($files[$year])) {
+            $this->abort('No import files found in data' . DS . $year);
+        }
+
+        $fileKeys = range(1, count($files[$year]));
+        $fileKey = null;
+        while (!is_numeric($fileKey) || !in_array($fileKey - 1, $fileKeys)) {
+            $this->out('Import files for year ' . $year . ':');
+            $tableData = $files[$year];
+            foreach ($tableData as $key => &$file) {
+                array_unshift($file, $key + 1);
+            }
+            array_unshift($tableData, ['Key', 'File', 'Imported']);
+            $this->helper('Table')->output($tableData);
+            $fileKey = $this->in('Select a file (' . min($fileKeys) . '-' . max($fileKeys) . '):');
+        }
+
+        return (int)$fileKey;
+    }
+
+    /**
+     * @param null|string $year Year/subdirectory to read from
+     * @param null|string $fileKey Key of the file to read from
+     * @return void
+     */
+    public function run($year = null, $fileKey = null)
+    {
+        if (!$year) {
+            $year = $this->selectYear();
+        }
+
+        if (!$fileKey) {
+            $fileKey = $this->selectFileKey($year);
+        }
+
+        // TODO: Process file
     }
 }
