5e78c026a9be6294d167f124c56fdc882d111ca9
Graham Watson <graham@phantomwatson.com>
2018-03-29 13:54:40 -0400

    Use file reader instead of load()
    So we can setReadDataOnly() and save some CPU cycles

--- src/Import/ImportFile.php
+++ src/Import/ImportFile.php
@@ -2,6 +2,7 @@
 namespace App\Import;
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Reader\BaseReader;
 use PhpOffice\PhpSpreadsheet\Reader\Exception;
 
 class ImportFile
@@ -12,8 +13,12 @@ class ImportFile
     public function __construct($year, $filename)
     {
         try {
+            $type = 'Xlsx';
             $path = ROOT . DS . 'data' . DS . $year . DS . $filename;
-            $this->spreadsheet = IOFactory::load($path);
+            /** @var BaseReader $reader */
+            $reader = IOFactory::createReader($type);
+            $reader->setReadDataOnly(true);
+            $this->spreadsheet = $reader->load($path);
         } catch (Exception $e) {
             $this->error = $e->getMessage();
         }
