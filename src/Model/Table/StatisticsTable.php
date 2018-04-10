2800f93364ea3bfc87be715c7a815a73ceefa25a
Graham Watson <graham@phantomwatson.com>
2018-04-09 20:21:34 -0400

    Add getValue()

--- src/Model/Table/StatisticsTable.php
+++ src/Model/Table/StatisticsTable.php
@@ -1,7 +1,9 @@
 <?php
 namespace App\Model\Table;
 
+use Cake\Network\Exception\InternalErrorException;
 use Cake\ORM\Table;
+use Cake\ORM\TableRegistry;
 use Cake\Validation\Validator;
 
 class StatisticsTable extends Table
@@ -63,5 +65,62 @@ class StatisticsTable extends Table
             ->notEmpty('file');
 
         return $validator;
+    }
+
+    /**
+     * Returns a SchoolMetricsTable or SchoolDistrictMetricsTable
+     *
+     * @param string $context Either 'school' or 'district'
+     * @return \Cake\ORM\Table
+     * @throws InternalErrorException
+     */
+    public static function getContextTable($context)
+    {
+        switch ($context) {
+            case 'school':
+                return TableRegistry::get('SchoolStatistics');
+            case 'district':
+                return TableRegistry::get('SchoolDistrictStatistics');
+            default:
+                throw new InternalErrorException('Statistics context "' .  $context . '" not recognized');
+        }
+    }
+
+    /**
+     * Returns the value of a specified statistic
+     *
+     * @param string $context Either 'school' or 'district'
+     * @param int $metricId SchoolMetric ID or SchoolDistrictMetric ID
+     * @param int $locationId School ID or SchoolDistrict ID
+     * @param int $year Year to look up data for
+     * @return string|null
+     * @throws InternalErrorException
+     */
+    public static function getValue($context, $metricId, $locationId, $year)
+    {
+        $conditions = [
+            'metric_id' => $metricId,
+            'year' => $year
+        ];
+
+        switch ($context) {
+            case 'school':
+                $conditions['school_id'] = $locationId;
+                break;
+            case 'district':
+                $conditions['school_district_id'] = $locationId;
+                break;
+            default:
+                throw new InternalErrorException('Statistics context "' .  $context . '" not recognized');
+        }
+
+        $result = self::getContextTable($context)->find()
+            ->select(['value'])
+            ->where($conditions)
+            ->orderDesc('created')
+            ->enableHydration(false)
+            ->first();
+
+        return $result ? $result['value'] : null;
     }
 }
