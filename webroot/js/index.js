076e18adea9bcfd3102222635672d4ad322e2369
Graham Watson <graham@phantomwatson.com>
2018-04-26 16:56:52 -0400

    Make metricManager available on global scope

--- webroot/js/index.js
+++ webroot/js/index.js
@@ -1,5 +1,6 @@
-import $ from 'jquery';
 import '../css/cake.css';
 import '../css/style.css';
+import metricManager from './metricManager.js';
 
-window.$ = window.jQuery = require("jquery");
+window.$ = window.jQuery = require('jquery');
+window.metricManager = metricManager;
