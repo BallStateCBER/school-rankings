1a466b820fc3495ed2770390ce47a22d62376822
Graham Watson <graham@phantomwatson.com>
2018-04-26 18:30:36 -0400

    Load metric manager styles with bundled JS

--- webroot/js/index.js
+++ webroot/js/index.js
@@ -1,5 +1,5 @@
 import '../css/cake.css';
-import '../css/style.css';
+import '../css/metric-manager.scss';
 import metricManager from './metricManager.js';
 
 window.$ = window.jQuery = require('jquery');
