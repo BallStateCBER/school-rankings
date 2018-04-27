03a318c77dcc479d04d21ee4e6a79aca41a01ab6
Graham Watson <graham@phantomwatson.com>
2018-04-27 11:48:38 -0400

    Remove importing of metricManager stuff

--- webroot/js/index.js
+++ webroot/js/index.js
@@ -1,6 +1,3 @@
 import '../css/main.scss';
-import '../css/metric-manager.scss';
-import metricManager from './metricManager.js';
 
 window.$ = window.jQuery = require('jquery');
-window.metricManager = metricManager;
