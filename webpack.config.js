ae182f2b735b2dacd66c597aa952d1163b191473
Graham Watson <graham@phantomwatson.com>
2018-04-27 16:08:23 -0400

    Rename index.js to main.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: './webroot/js/index.js',
+    main: './webroot/js/main.js',
     metricManager: './webroot/js/metricManager.js',
   },
   output: {
