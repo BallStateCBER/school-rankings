24949535f03002febc9c075be910bcee1470e5bf
Graham Watson <graham@phantomwatson.com>
2018-04-27 16:48:40 -0400

    Add /api prefix

--- config/routes.php
+++ config/routes.php
@@ -81,6 +81,10 @@ Router::prefix('admin', function (RouteBuilder $routes) {
     $routes->fallbacks(DashedRoute::class);
 });
 
+Router::prefix('api', function (RouteBuilder $routes) {
+    $routes->fallbacks(DashedRoute::class);
+});
+
 /**
  * Load all plugin routes. See the Plugin documentation on
  * how to customize the loading of plugin routes.
