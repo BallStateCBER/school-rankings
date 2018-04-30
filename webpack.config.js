fe39301770a40ea00c515708e0c5c5e7369693a6
Graham Watson <graham@phantomwatson.com>
2018-04-30 15:14:39 -0400

    Use copy-webpack-plugin to automatically copy jsTree files into webroot

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -51,6 +52,12 @@ module.exports = {
       'jQuery': 'jquery',
       'window.jQuery': 'jquery',
     }),
+    new CopyWebpackPlugin([
+      {
+        from: 'node_modules/jstree/dist',
+        to: 'webroot/jstree',
+      },
+    ]),
   ],
   resolve: {
     alias: {
