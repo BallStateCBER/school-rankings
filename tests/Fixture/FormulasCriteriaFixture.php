8942abf29debc294b4412253c4adb58ff4528742
Graham Watson <graham@phantomwatson.com>
2018-05-02 17:43:46 -0400

    Bake FormulasCriteriaFixture

---
+++ tests/Fixture/FormulasCriteriaFixture.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * FormulasCriteriaFixture
+ *
+ */
+class FormulasCriteriaFixture extends TestFixture
+{
+
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    public $table = 'formulas_criteria';
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'formula_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'criterion_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'utf8mb4_general_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'id' => 1,
+            'formula_id' => 1,
+            'criterion_id' => 1
+        ],
+    ];
+}
