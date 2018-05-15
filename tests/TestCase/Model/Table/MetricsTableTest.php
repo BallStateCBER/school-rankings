599bfbe7c9f31da02362864f8e66832cba19f150
Graham Watson <graham@phantomwatson.com>
2018-05-15 19:08:26 -0400

    Add (failing) testDeleteFailHasChildren()

--- tests/TestCase/Model/Table/MetricsTableTest.php
+++ tests/TestCase/Model/Table/MetricsTableTest.php
@@ -31,8 +31,10 @@ class MetricsTableTest extends TestCase
      * @var array
      */
     public $fixtures = [
-        'app.school_metrics',
         'app.school_district_metrics',
+        'app.school_district_statistics',
+        'app.school_metrics',
+        'app.school_statistics'
     ];
 
     /**
@@ -101,6 +103,21 @@ class MetricsTableTest extends TestCase
             ]);
             $result = $table->save($metric);
             $this->assertFalse($result);
+        }
+    }
+
+    /**
+     * Test that parent metrics cannot be deleted
+     *
+     * @return void
+     */
+    public function testDeleteFailHasChildren()
+    {
+        foreach ([$this->SchoolMetrics, $this->SchoolDistrictMetrics] as $table) {
+            $metricId = 1;
+            $metric = $table->get($metricId);
+            $result = $table->delete($metric);
+            $this->assertFalse($result, 'Metric with children can be deleted');
         }
     }
 }
