deefdc0494d9cc8975754675deaafec89bc898b2
Graham Watson <graham@phantomwatson.com>
2018-05-24 18:43:01 -0400

    Remove bundling of Font Awesome
    Since it's currently just being loaded via CDN <script> tag

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -1,9 +1,3 @@
 import '../css/main.scss';
 window.$ = window.jQuery = require('jquery');
 import 'bootstrap';
-import fontawesome from '@fortawesome/fontawesome';
-import solid from '@fortawesome/fontawesome-free-solid';
-import regular from '@fortawesome/fontawesome-free-regular';
-
-fontawesome.library.add(solid);
-fontawesome.library.add(regular);
