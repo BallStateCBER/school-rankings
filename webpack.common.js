515d43376ee3ad4e8dca8e9e373337eca8187e54
Graham Watson <graham@phantomwatson.com>
2018-05-24 17:48:56 -0400

    Load entire Font Awesome regular and solid icon set in all pages
    Should be refactored down to only what's necessary

--- webpack.common.js
+++ webpack.common.js
@@ -48,6 +48,14 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+      },
     ],
   },
   plugins: [
--- webroot/js/main.js
+++ webroot/js/main.js
@@ -1,3 +1,9 @@
 import '../css/main.scss';
 window.$ = window.jQuery = require('jquery');
 import 'bootstrap';
+import fontawesome from '@fortawesome/fontawesome';
+import solid from '@fortawesome/fontawesome-free-solid';
+import regular from '@fortawesome/fontawesome-free-regular';
+
+fontawesome.library.add(solid);
+fontawesome.library.add(regular);
