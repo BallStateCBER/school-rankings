4fbdf3a2a612c7c46ed59d3fdce264e554b62e16
Graham Watson <graham@phantomwatson.com>
2018-06-04 12:45:44 -0400

    Refactor, fix comment, remove unnecessary code

--- tests/TestCase/Model/Table/CriteriaTableTest.php
+++ tests/TestCase/Model/Table/CriteriaTableTest.php
@@ -27,33 +27,7 @@ class CriteriaTableTest extends TestCase
         'app.criteria',
         'app.school_district_metrics',
         'app.school_metrics',
-        'app.formulas',
-        'app.users',
-        'app.rankings',
-        'app.school_types',
-        'app.schools',
-        'app.school_districts',
-        'app.school_district_statistics',
-        'app.rankings_school_districts',
-        'app.cities',
-        'app.states',
-        'app.counties',
-        'app.cities_counties',
-        'app.rankings_counties',
-        'app.school_districts_counties',
-        'app.schools_counties',
-        'app.rankings_states',
-        'app.school_districts_states',
-        'app.schools_states',
-        'app.rankings_cities',
-        'app.school_districts_cities',
-        'app.schools_cities',
-        'app.school_statistics',
-        'app.school_levels',
-        'app.schools_school_levels',
-        'app.ranges',
-        'app.rankings_ranges',
-        'app.shared_formulas'
+        'app.formulas'
     ];
 
     /**
@@ -81,58 +55,36 @@ class CriteriaTableTest extends TestCase
     }
 
     /**
-     * Test initialize method
-     *
-     * @return void
-     */
-    public function testInitialize()
-    {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
-     * Test validationDefault method
-     *
-     * @return void
-     */
-    public function testValidationDefault()
-    {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
      * Tests that adding a criterion fails when metric_id is invalid
      *
      * @return void
      */
     public function testAddFailInvalidMetric()
     {
-        $table = TableRegistry::getTableLocator()->get('Criteria');
-        $criterion = $table->newEntity([
+        $criterion = $this->Criteria->newEntity([
             'formula_id' => 1,
             'metric_id' => 999999,
             'preference' => 'high',
             'weight' => 1
         ]);
-        $result = $table->save($criterion);
+        $result = $this->Criteria->save($criterion);
         $this->assertFalse($result);
     }
 
     /**
-     * Tests that adding a criterion fails when metric_id is invalid
+     * Tests successful adding
      *
      * @return void
      */
     public function testAddSuccess()
     {
-        $table = TableRegistry::getTableLocator()->get('Criteria');
-        $criterion = $table->newEntity([
+        $criterion = $this->Criteria->newEntity([
             'formula_id' => 1,
             'metric_id' => 1,
             'preference' => 'high',
             'weight' => 1
         ]);
-        $result = $table->save($criterion);
+        $result = $this->Criteria->save($criterion);
         $this->assertInstanceOf('App\Model\Entity\Criterion', $result);
     }
 }
