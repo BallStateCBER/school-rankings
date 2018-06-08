b22dea651c0533179dbeb4019d9583e3f78683db
Graham Watson <graham@phantomwatson.com>
2018-06-08 18:08:08 -0400

    Replace context-specific stat table test classes with single class

---
+++ tests/TestCase/Model/Table/StatisticsTableTest.php
@@ -0,0 +1,88 @@
+<?php
+namespace App\Test\TestCase\Model\Table;
+
+use App\Model\Table\StatisticsTable;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Model\Table\StatisticsTable Test Case
+ */
+class StatisticsTableTest extends TestCase
+{
+
+    /**
+     * Test subject
+     *
+     * @var \App\Model\Table\StatisticsTable
+     */
+    public $Statistics;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.school_statistics',
+        'app.school_district_metrics',
+        'app.school_metrics',
+        'app.schools',
+        'app.school_districts',
+        'app.school_district_statistics'
+    ];
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $config = TableRegistry::getTableLocator()->exists('Statistics') ? [] : ['className' => StatisticsTable::class];
+        $this->Statistics = TableRegistry::getTableLocator()->get('Statistics', $config);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Statistics);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Tests validation
+     *
+     * @return void
+     */
+    public function testValidation()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Tests application rules
+     *
+     * @return void
+     */
+    public function testRules()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Tests that float values are properly rounded to five decimal places when saved to the database
+     *
+     * @return void
+     */
+    public function testRoundingFloat()
+    {
+        $this->markTestIncomplete();
+    }
+}
