bd7ceeb6c02659d7cfcf8285a820ca3f69a482ac
Graham Watson <graham@phantomwatson.com>
2018-06-12 16:03:46 -0400

    Add Context class

---
+++ src/Model/Context/Context.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Model\Context;
+
+use Cake\Http\Exception\InternalErrorException;
+
+/**
+ * Class Context
+ *
+ * Used for managing the separation of school metrics and statistics versus district metrics and statistics
+ *
+ * @package App\Model\Context
+ */
+class Context
+{
+    /**
+     * Returns an array of all valid data contexts
+     *
+     * @return array
+     */
+    public static function getContexts()
+    {
+        return ['school', 'district'];
+    }
+
+    /**
+     * Returns whether or not the provided context is valid
+     *
+     * @param string $context Data context, such as 'school' or 'district'
+     * @return bool
+     */
+    public static function isValid($context)
+    {
+        return in_array($context, self::getContexts());
+    }
+
+    /**
+     * Returns the database field name associated with appropriate location ID
+     *
+     * @param string $context Data context
+     * @return mixed
+     */
+    public static function getLocationField($context)
+    {
+        $locationFields = [
+            'school' => 'school_id',
+            'district' => 'school_district_id'
+        ];
+
+        if (array_key_exists($context, $locationFields)) {
+            return $locationFields[$context];
+        }
+
+        throw new InternalErrorException('Unrecognized context: ' . $context);
+    }
+}
