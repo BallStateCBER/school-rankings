e9ede8de382d13a6de14798a9b16b27c04b18612
Graham Watson <graham@phantomwatson.com>
2018-07-23 16:37:25 -0400

    Replace rankings/generate endpoint with rankings/add
    Adds an unprocessed ranking entity to the database, to hopefully be
    processed by a background task soon thereafter.

--- src/Controller/Api/RankingsController.php
+++ src/Controller/Api/RankingsController.php
@@ -2,25 +2,55 @@
 namespace App\Controller\Api;
 
 use App\Controller\AppController;
-use App\Ranking\Ranking;
+use App\Model\Entity\Ranking;
+use App\Model\Table\RankingsTable;
 use Cake\Database\Expression\QueryExpression;
 use Cake\Http\Exception\BadRequestException;
+use Cake\Log\Log;
 use Cake\ORM\TableRegistry;
+use Cake\Utility\Hash;
 
+/**
+ * Class RankingsController
+ * @package App\Controller\Api
+ * @property RankingsTable $rankingsTable
+ */
 class RankingsController extends AppController
 {
+    private $rankingsTable;
+
     /**
-     * Generates and returns a ranking according to provided formula parameters
+     * Initialization method
+     *
+     * @return void
+     */
+    public function initialize()
+    {
+        parent::initialize();
+        $this->rankingsTable = TableRegistry::getTableLocator()->get('Rankings');
+    }
+
+    /**
+     * Adds a ranking record to be processed by a background task
      *
      * @return void
      * @throws \Exception
      * @throws BadRequestException
      */
-    public function generate()
+    public function add()
     {
         $context = $this->request->getData('context');
+
+        $ranking = $this->rankingsTable->newEntity([
+            'user_id' => null,
+            'formula_id' => $this->request->getData('formulaId'),
+            'school_type_id' => null,
+            'for_school_districts' => $context == 'district',
+            'hash' => RankingsTable::generateHash()
+        ]);
+
         $countyIds = [$this->request->getData('countyId')];
-        $locations = TableRegistry::getTableLocator()
+        $ranking->counties = TableRegistry::getTableLocator()
             ->get('Counties')
             ->find()
             ->where([
@@ -28,32 +58,44 @@ class RankingsController extends AppController
                     return $exp->in('id', $countyIds);
                 }
             ])
-            ->all();
-        $criteriaData = $this->request->getData('criteria');
-        if (empty($criteriaData)) {
-            throw new BadRequestException('Criteria data missing');
-        }
-        $criteria = [];
-        $criteriaTable = TableRegistry::getTableLocator()->get('Criteria');
-        foreach ($criteriaData as $criterionData) {
-            $criteria[] = $criteriaTable->newEntity([
-                'formula_id' => null,
-                'metric_id' => $criterionData['metric']['metricId'],
-                'weight' => 100,
-                'preference' => 'high'
-            ]);
-        }
-        $ranking = new Ranking([
-            'context' => $context,
-            'locations' => $locations,
-            'criteria' => $criteria
-        ]);
+            ->toArray();
 
-        $ranking = $ranking->getRanking();
+        $success = (bool)$this->rankingsTable->save($ranking);
+        if ($success) {
+            $id = $ranking->id;
+        } else {
+            $this->logRankingError($ranking);
+            $id = null;
+        }
 
         $this->set([
-            '_serialize' => ['ranking'],
-            'ranking' => $ranking
+            '_serialize' => ['id', 'success'],
+            'id' => $id,
+            'success' => $success
         ]);
+    }
+
+    /**
+     * Logs error in creating ranking
+     *
+     * @param Ranking $ranking Ranking entity
+     * @return void
+     */
+    private function logRankingError($ranking)
+    {
+        $errors = $ranking->getErrors();
+        $passesRules = $this->rankingsTable->checkRules($ranking, 'create');
+        if ($errors || !$passesRules) {
+            $errorMsg = 'There was an error creating that ranking.';
+            if ($errors) {
+                foreach (Hash::flatten($errors) as $field => $errorMsg) {
+                    $errorMsg .= "\n - $errorMsg ($field)";
+                }
+            }
+            if (!$passesRules) {
+                $errorMsg .= "\n - Did not pass application rules";
+            }
+            Log::write('error', $errorMsg);
+        }
     }
 }
