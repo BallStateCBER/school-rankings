2084137fa48eb3d3fb7cbb613f5099cbef46fd8e
Graham Watson <graham@phantomwatson.com>
2018-08-01 17:09:57 -0400

    Revert "Add rankings belongsToMany statistics association"
    This reverts commit b552dee

--- src/Model/Table/StatisticsTable.php
+++ src/Model/Table/StatisticsTable.php
@@ -33,11 +33,6 @@ class StatisticsTable extends Table
             'foreignKey' => 'school_district_id',
             'joinType' => 'INNER'
         ]);
-        $this->belongsToMany('Rankings', [
-            'foreignKey' => 'statistic_id',
-            'targetForeignKey' => 'ranking_id',
-            'joinTable' => 'ranking_results_statistics'
-        ]);
     }
 
     /**
