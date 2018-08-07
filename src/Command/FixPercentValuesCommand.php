26650043e5fff3d0a503a5351402e5c232d56fb3
Graham Watson <graham@phantomwatson.com>
2018-08-07 17:21:07 -0400

    Refactor

--- src/Command/FixPercentValuesCommand.php
+++ src/Command/FixPercentValuesCommand.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Command;
 
+use App\Model\Table\MetricsTable;
+use App\Model\Table\StatisticsTable;
 use Cake\Console\Arguments;
 use Cake\Console\Command;
 use Cake\Console\ConsoleIo;
@@ -13,9 +15,27 @@ use Cake\Utility\Hash;
  * Class FixPercentValuesCommand
  * @package App\Command
  * @property ProgressHelper $progress
+ * @property int $flaggedMetricsCount;
+ * @property ConsoleIo $io;
+ * @property int $metricCount;
+ * @property array $metrics;
+ * @property MetricsTable $metricsTable;
+ * @property int $statisticsCount;
+ * @property StatisticsTable $statisticsTable;
+ * @property string $updateResponse;
  */
 class FixPercentValuesCommand extends Command
 {
+    private $flaggedMetricsCount;
+    private $io;
+    private $metricCount;
+    private $metrics;
+    private $metricsTable;
+    private $progress;
+    private $statisticsCount;
+    private $statisticsTable;
+    private $updateResponse;
+
     /**
      * Fixes statistic values like "0.025" that should be stored as "2.5%"
      *
@@ -26,10 +46,52 @@ class FixPercentValuesCommand extends Command
      */
     public function execute(Arguments $args, ConsoleIo $io)
     {
-        $io->out('Finding percentage metrics...');
-        $metricsTable = TableRegistry::getTableLocator()->get('Metrics');
-        /** @var array $metrics */
-        $metrics = $metricsTable->find()
+        $this->io = $io;
+        $this->metricsTable = TableRegistry::getTableLocator()->get('Metrics');
+        $this->progress = $this->io->helper('Progress');
+        $this->statisticsTable = TableRegistry::getTableLocator()->get('Statistics');
+
+        $this->getMetrics();
+        $this->showMetrics();
+
+        if (!$this->metrics) {
+            return;
+        }
+
+        $this->findStatistics();
+
+        if ($this->flaggedMetricsCount) {
+            $this->io->overwrite(sprintf(
+                ' - %s %s found for %s %s',
+                number_format($this->statisticsCount),
+                __n('statistic', 'statistics', $this->statisticsCount),
+                number_format($this->flaggedMetricsCount),
+                __n('metric', 'metrics', $this->flaggedMetricsCount)
+            ));
+        } else {
+            $this->io->overwrite(' - No statistics need updated');
+
+            return;
+        }
+
+        $this->updateResponse = $this->io->askChoice('Update statistics?', ['y', 'n', 'dry run'], 'y');
+        if ($this->updateResponse == 'n') {
+            return;
+        }
+
+        $this->updateStatistics();
+    }
+
+    /**
+     * Gets metrics whose statistics should be formatted as percentages
+     *
+     * @throws \Aura\Intl\Exception
+     * @return void
+     */
+    private function getMetrics()
+    {
+        $this->io->out('Finding percentage metrics...');
+        $this->metrics = $this->metricsTable->find()
             ->select(['id', 'name'])
             ->where([
                 'OR' => [
@@ -46,16 +108,24 @@ class FixPercentValuesCommand extends Command
             ])
             ->orderAsc('name')
             ->toArray();
-        $metricCount = count($metrics);
-        $io->out(sprintf(
+        $this->metricCount = count($this->metrics);
+        $this->io->out(sprintf(
             ' - %s %s found',
-            $metricCount,
-            __n('metric', 'metrics', $metricCount)
+            $this->metricCount,
+            __n('metric', 'metrics', $this->metricCount)
         ));
+    }
 
-        $updateResponse = $io->askChoice('List metric names?', ['y', 'n'], 'n');
-        if ($updateResponse == 'y') {
-            $names = Hash::extract($metrics, '{n}.name');
+    /**
+     * Optionally shows a list of metrics found
+     *
+     * @return void
+     */
+    private function showMetrics()
+    {
+        $this->updateResponse = $this->io->askChoice('List metric names?', ['y', 'n'], 'n');
+        if ($this->updateResponse == 'y') {
+            $names = Hash::extract($this->metrics, '{n}.name');
             $names = array_values(array_unique($names));
             $count = count($names);
             for ($n = 0; $n < $count; $n += 50) {
@@ -65,33 +135,34 @@ class FixPercentValuesCommand extends Command
                     }
                     $name = $names[$n + $i];
                     $name = str_replace("\n", "\n   ", $name);
-                    $io->out(' - ' . $name);
+                    $this->io->out(' - ' . $name);
                 }
-                $updateResponse = $io->askChoice('Show more?', ['y', 'n'], 'y');
-                if ($updateResponse == 'n') {
+                $this->updateResponse = $this->io->askChoice('Show more?', ['y', 'n'], 'y');
+                if ($this->updateResponse == 'n') {
                     break;
                 }
             }
         }
+    }
 
-        if (!$metrics) {
-            return;
-        }
-
-        $io->out();
-        $io->out('Finding statistics that need reformatted...');
-        /** @var ProgressHelper $progress */
-        $progress = $io->helper('Progress');
-        $progress->init([
-            'total' => $metricCount,
+    /**
+     * Finds incorrectly formatted statistics that should be formatted as percentages
+     *
+     * @return void
+     */
+    private function findStatistics()
+    {
+        $this->io->out();
+        $this->io->out('Finding statistics that need reformatted...');
+        $this->progress->init([
+            'total' => $this->metricCount,
             'width' => 40,
         ]);
-        $progress->draw();
-        $statsTable = TableRegistry::getTableLocator()->get('Statistics');
+        $this->progress->draw();
         $flaggedMetricsCount = 0;
         $statisticsCount = 0;
-        foreach ($metrics as &$metric) {
-            $metric['statistics'] = $statsTable->find()
+        foreach ($this->metrics as &$metric) {
+            $metric['statistics'] = $this->statisticsTable->find()
                 ->select(['id', 'value'])
                 ->where([
                     'metric_id' => $metric['id'],
@@ -105,61 +176,50 @@ class FixPercentValuesCommand extends Command
                 $flaggedMetricsCount++;
                 $statisticsCount += $metric['statistics']->count();
             }
-            $progress->increment(1)->draw();
+            $this->progress->increment(1)->draw();
         }
+    }
 
-        if ($flaggedMetricsCount) {
-            $io->overwrite(sprintf(
-                ' - %s %s found for %s %s',
-                number_format($statisticsCount),
-                __n('statistic', 'statistics', $statisticsCount),
-                number_format($flaggedMetricsCount),
-                __n('metric', 'metrics', $flaggedMetricsCount)
-            ));
-        } else {
-            $io->overwrite(' - No statistics need updated');
-
-            return;
-        }
-
-        $updateResponse = $io->askChoice('Update statistics?', ['y', 'n', 'dry run'], 'y');
-        if ($updateResponse == 'n') {
-            return;
-        }
-
-        $io->out();
-        $io->out('Updating statistics...');
-        $progress->init([
-            'total' => $statisticsCount,
+    /**
+     * Updates statistics (or performs a dry run)
+     *
+     * @return void
+     */
+    private function updateStatistics()
+    {
+        $this->io->out();
+        $this->io->out('Updating statistics...');
+        $this->progress->init([
+            'total' => $this->statisticsCount,
             'width' => 40,
         ]);
-        $progress->draw();
-        foreach ($metrics as $metric) {
+        $this->progress->draw();
+        foreach ($this->metrics as $metric) {
             if (!$metric['statistics']) {
                 continue;
             }
 
             foreach ($metric['statistics'] as $statistic) {
-                $progress->increment(1)->draw();
+                $this->progress->increment(1)->draw();
                 if (!is_numeric($statistic->value)) {
-                    $io->overwrite('Skipping non-numeric value ' . $statistic->value);
+                    $this->io->overwrite('Skipping non-numeric value ' . $statistic->value);
                     continue;
                 }
 
                 $originalValue = $statistic->value;
                 $value = round(($originalValue * 100), 2) . '%';
-                $statistic = $statsTable->patchEntity($statistic, compact('value'));
-                if ($updateResponse == 'dry run') {
+                $statistic = $this->statisticsTable->patchEntity($statistic, compact('value'));
+                if ($this->updateResponse == 'dry run') {
                     if ($statistic->getErrors()) {
-                        $io->overwrite('');
-                        $io->error('Error updating ' . $originalValue . ' to ' . $value);
+                        $this->io->overwrite('');
+                        $this->io->error('Error updating ' . $originalValue . ' to ' . $value);
                         print_r($statistic->getErrors());
                     } else {
-                        $io->overwrite('Would update ' . $originalValue . ' to ' . $value);
+                        $this->io->overwrite('Would update ' . $originalValue . ' to ' . $value);
                     }
-                } elseif (!$statsTable->save($statistic)) {
-                    $io->overwrite('Error updating statistic. Details: ');
-                    $io->out();
+                } elseif (!$this->statisticsTable->save($statistic)) {
+                    $this->io->overwrite('Error updating statistic. Details: ');
+                    $this->io->out();
                     print_r($statistic->getErrors());
 
                     return;
