de26017687392ce245476c226accd02d9bd591de
Graham Watson <graham@phantomwatson.com>
2018-09-14 12:16:04 -0400

    Upgrade Application class with CakePHP skeleton app code

--- src/Application.php
+++ src/Application.php
@@ -29,8 +29,10 @@ use App\Command\MetricReparentCommand;
 use App\Command\MetricTreeCleanCommand;
 use App\Shell\RankTestShell;
 use Cake\Core\Configure;
+use Cake\Core\Exception\MissingPluginException;
 use Cake\Error\Middleware\ErrorHandlerMiddleware;
 use Cake\Http\BaseApplication;
+use Cake\Http\Middleware\CsrfProtectionMiddleware;
 use Cake\Routing\Middleware\AssetMiddleware;
 use Cake\Routing\Middleware\RoutingMiddleware;
 
@@ -43,18 +45,31 @@ use Cake\Routing\Middleware\RoutingMiddleware;
 class Application extends BaseApplication
 {
     /**
-     * Application bootstrap method
-     *
-     * @return void
+     * {@inheritDoc}
      */
     public function bootstrap()
     {
+        // Call parent to load bootstrap from files.
         parent::bootstrap();
 
         $this->addPlugin('Queue');
 
+        if (PHP_SAPI === 'cli') {
+            try {
+                $this->addPlugin('Bake');
+            } catch (MissingPluginException $e) {
+                // Do not halt if the plugin is missing
+            }
+
+            $this->addPlugin('Migrations');
+        }
+
+        /*
+         * Only try to load DebugKit in development mode
+         * Debug Kit should not be installed on a production system
+         */
         if (Configure::read('debug')) {
-            $this->addPlugin('DebugKit');
+            $this->addPlugin(\DebugKit\Plugin::class);
         }
     }
 
@@ -72,10 +87,20 @@ class Application extends BaseApplication
             ->add(ErrorHandlerMiddleware::class)
 
             // Handle plugin/theme assets like CakePHP normally does.
-            ->add(AssetMiddleware::class)
+            ->add(new AssetMiddleware([
+                'cacheTime' => Configure::read('Asset.cacheTime')
+            ]))
 
             // Add routing middleware.
-            ->add(new RoutingMiddleware($this));
+            // Routes collection cache enabled by default, to disable route caching
+            // pass null as cacheConfig, example: `new RoutingMiddleware($this)`
+            // you might want to disable this cache in case your routing is extremely simple
+            ->add(new RoutingMiddleware($this, '_cake_routes_'))
+
+            // Add csrf middleware.
+            ->add(new CsrfProtectionMiddleware([
+                'httpOnly' => true
+            ]));
 
         return $middlewareQueue;
     }
