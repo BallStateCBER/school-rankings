e32f97e17b5d2f57e7e295153ea97b999c10eec3
Graham Watson <graham@phantomwatson.com>
2018-09-20 19:03:40 -0400

    Add generic Checkbox and CheckboxContainer modules

---
+++ webroot/js/formula-form/checkbox-container.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Checkbox from './checkbox.jsx';
+
+class CheckboxContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checkedItems: new Map(),
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const item = e.target.name;
+    const isChecked = e.target.checked;
+    this.setState((prevState) => ({
+      checkedItems: prevState.checkedItems.set(item, isChecked),
+    }));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {
+          this.props.checkboxes.map((item) => (
+              <div className="form-check" key={item.key}>
+                <Checkbox name={item.name}
+                          checked={this.state.checkedItems.get(item.name)}
+                          onChange={this.handleChange} id={item.key} />
+                <label className="form-check-label" htmlFor={item.key}>
+                  {item.label}
+                </label>
+              </div>
+          ))
+        }
+      </React.Fragment>
+    );
+  }
+}
+
+CheckboxContainer.propTypes = {
+  checkboxes: PropTypes.array.isRequired,
+};
+
+export default CheckboxContainer;
---
+++ webroot/js/formula-form/checkbox.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Checkbox = ({type = 'checkbox', name, checked = false, onChange, id}) => (
+    <input type={type} name={name} checked={checked} onChange={onChange}
+           className="form-check-input" id={id} />
+);
+
+Checkbox.propTypes = {
+  checked: PropTypes.bool,
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  type: PropTypes.string,
+};
+
+export default Checkbox;
