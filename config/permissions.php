0f10c693b5b3265254a59874aa623730d734f69b
Graham Watson <graham@phantomwatson.com>
2018-10-19 12:54:15 -0400

    Add missing controller/action values

--- config/permissions.php
+++ config/permissions.php
@@ -4,12 +4,24 @@ return [
         // Allow admins access to everything
         [
             'role' => 'admin',
+            'controller' => '*',
+            'action' => '*',
             'allowed' => true
         ],
 
         // Allow everyone access to any non-prefixed actions
         [
             'prefix' => false,
+            'plugin' => false,
+            'controller' => '*',
+            'action' => '*',
+            'allowed' => true
+        ],
+
+        [
+            'plugin' => 'CakeDC/Users',
+            'controller' => 'Users',
+            'action' => 'logout',
             'allowed' => true
         ]
     ],
