afcf9d167aa2a6c914c9b8ae587f77359a010fe7
Graham Watson <graham@phantomwatson.com>
2018-10-22 17:52:44 -0400

    Add test placeholders to test

--- tests/TestCase/Controller/UsersControllerTest.php
+++ tests/TestCase/Controller/UsersControllerTest.php
@@ -16,8 +16,49 @@ class UsersControllerTest extends IntegrationTestCase
      */
     public $fixtures = [
         'app.users',
+        'app.social_accounts',
         'app.formulas',
         'app.rankings',
         'app.shared_formulas'
     ];
+
+    /**
+     * Test initialize method
+     *
+     * @return void
+     */
+    public function testInitialize()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test register method
+     *
+     * @return void
+     */
+    public function testRegister()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test login method
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test logout method
+     *
+     * @return void
+     */
+    public function testLogout()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
 }
