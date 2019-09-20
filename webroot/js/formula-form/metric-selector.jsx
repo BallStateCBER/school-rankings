eeb87633eda17fc879322ad9f3bb6b91eb44cd4b
Graham Watson <graham@phantomwatson.com>
2019-09-20 16:12:11 -0400

    Refactor with new MetricSorter class

--- webroot/js/formula-form/metric-selector.jsx
+++ webroot/js/formula-form/metric-selector.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import {Formatter} from '../metric-manager/formatter.js';
 import {Button} from 'reactstrap';
 import 'jstree';
+import {MetricSorter} from '../sort/metric-sorter.js';
 
 class MetricSelector extends React.Component {
   constructor(props) {
@@ -120,60 +121,8 @@ class MetricSelector extends React.Component {
         show_only_matches: true,
         show_only_matches_children: true,
       },
-
-      /**
-       * Returns 1 if node1 should be placed after node2, or -1 otherwise
-       * @param {Object} node1
-       * @param {Object} node2
-       * @return {number}
-       */
       sort: function(node1, node2) {
-        const metric1 = this.get_node(node1).text;
-        const metric2 = this.get_node(node2).text;
-
-        // Order grades
-        const gradeKeyword = 'Grade ';
-        const metric1IsGrade = metric1.indexOf(gradeKeyword) === 0;
-        const metric2IsGrade = metric2.indexOf(gradeKeyword) === 0;
-        if (metric1IsGrade && metric2IsGrade) {
-          const grade1 = metric1.substring(gradeKeyword.length);
-          const grade2 = metric2.substring(gradeKeyword.length);
-
-          // Sort "Grade 12+..." after other "Grade..." metrics
-          if (grade1.indexOf('12+') !== -1) {
-            return 1;
-          }
-          if (grade2.indexOf('12+') !== -1) {
-            return -1;
-          }
-
-          // Sort grades numerically, if possible
-          const grade1Num = parseFloat(grade1);
-          const grade2Num = parseFloat(grade2);
-          if (!isNaN(grade1Num) && !isNaN(grade2Num)) {
-            return grade1Num > grade2Num ? 1 : -1;
-          }
-        }
-
-        // Order "Total..." metrics after "Grade..." metrics
-        if (metric1IsGrade) {
-          return metric2.indexOf('Total') !== -1 ? -1 : 1;
-        }
-        if (metric2IsGrade) {
-          return metric1.indexOf('Total') !== -1 ? 1 : -1;
-        }
-
-        // Order "Pre-K" before "Kindergarten"
-        const kgKeyword = 'Kindergarten';
-        const pkKeyword = 'Pre-K';
-        if (metric1.indexOf(pkKeyword) === 0 && metric2.indexOf(kgKeyword) === 0) {
-          return -1;
-        }
-        if (metric2.indexOf(pkKeyword) === 0 && metric1.indexOf(kgKeyword) === 0) {
-          return 1;
-        }
-
-        return metric1 > metric2 ? 1 : -1;
+        return MetricSorter.sort(this, node1, node2);
       },
     };
   }
