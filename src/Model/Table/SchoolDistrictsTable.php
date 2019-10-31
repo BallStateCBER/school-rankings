74247320cbb2789b61b685ae341e30a9c22e3e3a
Graham Watson <graham@phantomwatson.com>
2019-10-31 19:21:58 -0400

    Refactor

--- src/Model/Table/SchoolDistrictsTable.php
+++ src/Model/Table/SchoolDistrictsTable.php
@@ -88,8 +88,8 @@ class SchoolDistrictsTable extends Table
     /**
      * Default validation rules.
      *
-     * @param \Cake\Validation\Validator $validator Validator instance.
-     * @return \Cake\Validation\Validator
+     * @param Validator $validator Validator instance.
+     * @return Validator
      */
     public function validationDefault(Validator $validator)
     {
@@ -120,8 +120,8 @@ class SchoolDistrictsTable extends Table
      * Returns a rules checker object that will be used for validating
      * application integrity.
      *
-     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
-     * @return \Cake\ORM\RulesChecker
+     * @param RulesChecker $rules The rules object to be modified.
+     * @return RulesChecker
      */
     public function buildRules(RulesChecker $rules)
     {
