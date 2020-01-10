c81cdf3f36be4562e6fac02607bc81381701b9a4
Graham Watson <graham@phantomwatson.com>
2020-01-10 17:28:57 -0500

    Add getMostRecentYear()

--- src/Model/Index/StatisticsIndex.php
+++ src/Model/Index/StatisticsIndex.php
@@ -1,8 +1,10 @@
 <?php
 namespace App\Model\Index;
 
+use App\Model\Entity\Statistic;
 use Cake\Core\Configure;
 use Cake\ElasticSearch\Index;
+use Cake\Http\Exception\InternalErrorException;
 
 class StatisticsIndex extends Index
 {
@@ -18,5 +20,31 @@ class StatisticsIndex extends Index
 
         $this->_name = Configure::read('Elasticsearch.statisticsIndex');
         $this->_type = '_doc';
+    }
+
+    /**
+     * Returns the most recent year for which statistics are found, optionally for a specified metric
+     *
+     * @param int|null $metricId Optional metric ID
+     * @return int|null
+     * @throws InternalErrorException
+     */
+    public function getMostRecentYear($metricId = null)
+    {
+        /** @var Statistic $stat */
+        $query = $this->find()
+            ->select(['year'])
+            ->order(['year' => 'asc']);
+
+        if ($metricId) {
+            if (!is_int($metricId)) {
+                throw new InternalErrorException('Metric ID ' . $metricId . ' is invalid.');
+            }
+            $query->where(['metric_id' => $metricId]);
+        }
+
+        $stat = $query->first();
+
+        return $stat ? $stat->year : null;
     }
 }
