6adfffc1e523721d648b76e48677291c74d5cdc4
Graham Watson <graham@phantomwatson.com>
2020-02-18 18:05:35 -0500

    Add unprocessed criterion weight inputs
    At the moment, this input is not actually read

--- webroot/js/formula-form/criterion.jsx
+++ webroot/js/formula-form/criterion.jsx
@@ -1,27 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import InputRange from 'react-input-range';
+import 'react-input-range/lib/css/index.css';
 
 class Criterion extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      value: 100,
+    };
+  }
+
+  getWeightLabel(value) {
+    if (value >= 80 && value <= 120) {
+      return 'Normal';
+    }
+    if (value < 80) {
+      return 'Less important';
+    }
+    if (value > 120) {
+      return 'More important';
+    }
   }
 
   render() {
     return (
       <tr>
         <td>
-          <button type="button" className="close float-right"
-                  aria-label="Close" onClick={this.props.onRemove}>
+          <button type="button" className="close float-right" aria-label="Close" onClick={this.props.onRemove}>
             <span aria-hidden="true">&times;</span>
           </button>
           <p className="metric-name">
             {this.props.name}
           </p>
-          <input type="hidden"
-                 name={'criteria[' + this.props.metricId + '][metricId]'}
-                 data-field="metricId"
+          <div className="row weight-input">
+            <div className="col-2">
+              Importance:
+            </div>
+            <div className="col-10">
+              <InputRange minValue={1} maxValue={200} value={this.state.value}
+                          formatLabel={(value) => this.getWeightLabel(value)}
+                          onChange={(value) => this.setState({value})} />
+            </div>
+          </div>
+          <input type="hidden" name={'criteria[' + this.props.metricId + '][metricId]'} data-field="metricId"
                  value={this.props.metricId} />
         </td>
       </tr>
