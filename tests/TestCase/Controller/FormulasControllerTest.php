c2c138a05d238801bbadc55e83df460aaf1e64b7
Graham Watson <graham@phantomwatson.com>
2020-04-07 18:30:57 -0400

    Remove unused baked tests and add testForm()

--- tests/TestCase/Controller/FormulasControllerTest.php
+++ tests/TestCase/Controller/FormulasControllerTest.php
@@ -17,60 +17,25 @@ class FormulasControllerTest extends TestCase
      * @var array
      */
     public $fixtures = [
-        'app.Formulas',
-        'app.Users',
-        'app.Rankings',
-        'app.SharedFormulas',
-        'app.Criteria',
+        'app.Counties',
+        'app.States',
+        'app.Grades',
+        'app.SchoolTypes',
     ];
 
     /**
-     * Test index method
+     * Tests that the home page can be loaded
      *
      * @return void
+     * @throws \PHPUnit\Exception
      */
-    public function testIndex()
+    public function testForm()
     {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
-     * Test view method
-     *
-     * @return void
-     */
-    public function testView()
-    {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
-     * Test add method
-     *
-     * @return void
-     */
-    public function testAdd()
-    {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
-     * Test edit method
-     *
-     * @return void
-     */
-    public function testEdit()
-    {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
-
-    /**
-     * Test delete method
-     *
-     * @return void
-     */
-    public function testDelete()
-    {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->get([
+            'controller' => 'Formulas',
+            'action' => 'form',
+        ]);
+        $this->assertResponseOk();
+        $this->assertResponseContains('<html lang="en">');
     }
 }
