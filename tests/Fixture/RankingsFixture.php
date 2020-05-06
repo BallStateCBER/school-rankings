28644b04052bca04d4d86f03978673aa11e742dc
Graham Watson <graham@phantomwatson.com>
2020-05-06 17:32:12 -0400

    Remove blank line

--- tests/Fixture/RankingsFixture.php
+++ tests/Fixture/RankingsFixture.php
@@ -45,7 +45,6 @@ class RankingsFixture extends TestFixture
                 'id' => 1,
                 'user_id' => 1,
                 'formula_id' => 1,
-
                 'for_school_districts' => 0,
                 'hash' => 'abc123',
                 'created' => '2018-07-27 20:43:37',
